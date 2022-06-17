f8888f7967515b92ae8c733ef3a66e4213679b67
= <=>
2022-06-17 23:35:20 +0700

    nambahin model Buku

---
+++ src/asistensi/models/Buku.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package asistensi.models;
+
+/**
+ *
+ * @author 62859
+ */
+public class Buku {
+    String judul;
+    String pengarang;
+    int jumlahHalaman;
+    String Penerbit;
+
+    public Buku(String judul, String pengarang, int jumlahHalaman, String Penerbit) {
+        this.judul = judul;
+        this.pengarang = pengarang;
+        this.jumlahHalaman = jumlahHalaman;
+        this.Penerbit = Penerbit;
+    }
+    
+    
+    
+}
